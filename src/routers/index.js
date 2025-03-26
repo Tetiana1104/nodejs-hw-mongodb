c98ab932297bf8764a70cf4b721b86a43288d190
Tetiana Korkh <tanyakorkh1104@gmail.com>
2025-03-26 14:57:54 +0200

    fix: remove duplicate / route

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,8 +7,4 @@ const router = Router();
 router.use('/contacts', contactsRouters);
 router.use('/auth', authRouter);
 
-router.get('/', (req, res) => {
-  res.json({ message: 'API is running!' });
-});
-
 export default router;
