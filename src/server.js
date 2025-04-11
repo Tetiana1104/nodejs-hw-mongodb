440ce8bf1adee8169b5fc8430f05852e56c927b1
Tetiana Korkh <tanyakorkh1104@gmail.com>
2025-04-12 00:50:34 +0300

    hw7: implement API documentation (Swagger + Redoc)

--- src/server.js
+++ src/server.js
@@ -1,26 +1,41 @@
 import cookieParser from 'cookie-parser';
+import * as fs from 'node:fs';
+import path from 'node:path';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import swaggerUiExpress from 'swagger-ui-express';
+
 import contactsRouters from './routers/contactsRouters.js';
 import authRouter from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve('docs', 'swagger.json'), 'utf-8'),
+);
+
 export function setupServer() {
   const app = express();
   const PORT = process.env.PORT || 3000;
 
+  app.use(
+    '/api-docs',
+    swaggerUiExpress.serve,
+    swaggerUiExpress.setup(swaggerDocument),
+  );
+
   app.use(cookieParser());
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
 
   app.use(cors());
   app.use(pino());
-
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'API is running!' });
-  });
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouters);
